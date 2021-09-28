17e99a0586d57b9ba763b912244d6d262dbda165
prem <premmahato7488@gmail.com>
2021-09-28 10:41:33 +0530

    first commmit

---
+++ script.js
@@ -0,0 +1,16 @@
+$(".cd").slideUp();
+$(".physicsHeading").click(function(){$(".pc").slideToggle();});
+
+$(".ch").click(function(){$(".cc").slideToggle();});
+
+$(".gh").click(function(){$(".gc").slideToggle();});
+
+$(".mh").click(function(){$(".mc").slideToggle();});
+
+$(".eh").click(function(){$(".ec").slideToggle();});
+
+$(".ggh").click(function(){$(".ggc").slideToggle();});
+
+
+
+
